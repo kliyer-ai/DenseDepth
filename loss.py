remove normalization and fix metrics

loss.py
@@ -1,9 +1,9 @@
 import keras.backend as K
 import tensorflow as tf
+from utils import normalize_disparity
 
 def depth_loss_function(y_true, y_pred):
     return ssim(y_true, y_pred) + edges(y_true, y_pred) + point_wise_depth(y_true, y_pred)
-
 
 def edges(y_true, y_pred):
     # Edges
@@ -21,3 +21,4 @@ def ssim(y_true, y_pred):
     # Structural similarity (SSIM) index
     l_ssim = K.clip((1 - tf.image.ssim(y_true, y_pred, 1.0)) * 0.5, 0, 1)
     return l_ssim
+

test.py
@@ -7,10 +7,18 @@ import matplotlib
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '5'
 from keras.models import model_from_yaml
 from layers import BilinearUpSampling2D
-from utils import predict, load_images, display_images
+from utils import predict, load_images, display_images, compute_errors
 from matplotlib import pyplot as plt
 import cv2 as cv
 import numpy as np
+from PIL import Image
+from skimage.transform import resize
+from shape import get_shape_depth
+
+def print_info(img):
+    print('min', img.min())
+    print('max', img.max())
+    print('mean', img.mean())
 
 # Argument Parser
 parser = argparse.ArgumentParser(description='High Quality Monocular Depth Estimation via Transfer Learning')
@@ -35,14 +43,25 @@ print('\nModel loaded ({0}).'.format(args.model))
 # Input images
 inputs = load_images( glob.glob(args.input) )
 
+# GT
+base_path, _ = os.path.split(args.input)
+gt = np.clip(np.asarray(Image.open( os.path.join(base_path, 'gt.png') ), dtype=float) / 255, 0, 1)
+gt = resize(gt, get_shape_depth(halved=True), preserve_range=True, mode='reflect', anti_aliasing=True )
+print_info(gt)
+
 # Compute results
-outputs = predict(model, inputs)
+output = predict(model, inputs)
+print_info(output)
+
+metrics = compute_errors(gt, np.reshape(output, get_shape_depth(halved=True)))
+print("{:>10}, {:>10}, {:>10}, {:>10}, {:>10}, {:>10}".format('a1', 'a2', 'a3', 'rel', 'rms', 'log_10'))
+print("{:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}".format(metrics[0],metrics[1],metrics[2],metrics[3],metrics[4],metrics[5]))
 
 #matplotlib problem on ubuntu terminal fix
 #matplotlib.use('TkAgg')   
 
 # Display results
-viz = display_images(outputs.copy(), inputs.copy(), is_colormap=False)
+viz = display_images(output.copy(), inputs.copy(), is_colormap=False)
 split_path = os.path.normpath(args.input).split('/')
 if len(split_path) < 2: path = 'test.png'
 else: path = split_path[-2] + '_pred.png'

utils.py
@@ -130,24 +130,20 @@ def load_test_data(test_data_zip_file, nr_inputs=1):
     return {'rgb':batches_x, 'depth':batch_y, 'crop':None}
 
 
-def normalize_disparity(y):
-    centered = y - np.min(y)
-    return centered / np.max(centered)
-
-
 def compute_errors(gt, pred):
-    gt = normalize_disparity(gt)
-    pred = normalize_disparity(pred)
-    eps = K.epsilon()
+    pred = np.clip(pred, 0 , 1)
+    gt = np.clip(gt, 0, 1)
+    pred = pred * 255 + 1
+    gt = gt * 255 + 1
 
-    thresh = np.maximum((gt / (pred+eps)), (pred / (gt+eps)))
+    thresh = np.maximum((gt / pred), (pred / gt))
     a1 = (thresh < 1.25   ).mean()
     a2 = (thresh < 1.25 ** 2).mean()
     a3 = (thresh < 1.25 ** 3).mean()
-    abs_rel = np.mean(np.abs(gt - pred) / (gt+eps))
+    abs_rel = np.mean(np.abs(gt - pred) / gt)
     rmse = (gt - pred) ** 2
     rmse = np.sqrt(rmse.mean())
-    log_10 = (np.abs(np.log10(gt+eps)-np.log10(pred+eps))).mean()
+    log_10 = (np.abs(np.log10(gt)-np.log10(pred))).mean()
     return a1, a2, a3, abs_rel, rmse, log_10
 
 def evaluate(model, rgb, depth, crop, batch_size=6, verbose=False):