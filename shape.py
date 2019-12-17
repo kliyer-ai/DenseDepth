c9e9c7bffa27dac6b66c00400a53004b1ee77f59
Nick Stracke <nick.stracke@web.de>
2019-12-17 12:06:58 +0100

    update shape

--- shape.py
+++ shape.py
@@ -1,12 +1,12 @@
 
 def get_shape_rgb(batch_size=None, halved=False):
-    dim = 224
+    dim = 320
     if halved: dim = dim // 2
     if batch_size: return (batch_size, dim, dim, 3)
     else: return  (dim, dim, 3)
 
 def get_shape_depth(batch_size=None, halved=False):
-    dim = 224
+    dim = 320
     if halved: dim = dim // 2
     if batch_size: return (batch_size, dim, dim, 1)
     else: return  (dim, dim, 1)
