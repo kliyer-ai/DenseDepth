9278cd95360c96716b8d7516b5440613cdacc285
Nick Stracke <nick.stracke@web.de>
2020-01-07 09:29:01 +0100

    fix dim changes

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import matplotlib
 
 # Keras / TensorFlow
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '5'
-from keras.models import model_from_yaml
+from keras.models import model_from_yaml, Model
 from layers import BilinearUpSampling2D
 from utils import predict, load_images, display_images, compute_errors
 from matplotlib import pyplot as plt
@@ -27,6 +27,8 @@ parser.add_argument('--model', default='nyu.h5', type=str, help='Trained Keras m
 parser.add_argument('--input', default='examples/*.png', type=str, help='Input filename or folder.')
 args = parser.parse_args()
 
+os.environ['CUDA_VISIBLE_DEVICES'] = '2'
+
 print('Loading model...')
 
 # Load model into GPU / CPU
@@ -36,22 +38,29 @@ with open(args.model+'.yaml', 'r') as f:
     model.load_weights(args.model+'_weights.h5')
 
 print('\nModel loaded ({0}).'.format(args.model))
+input_left = model.inputs[0]
+disp_left = model.get_layer('disp_left').get_output_at(0),
+simple_model = Model(input=input_left, output=disp_left)
 
 # Input images
 inputs = load_images( glob.glob(args.input) )
 
 # GT
 base_path, _ = os.path.split(args.input)
-gt = np.clip(np.asarray(Image.open( os.path.join(base_path, 'gt.png') ), dtype=float) / 255, 0, 1)
+# gt = np.clip(np.asarray(Image.open( os.path.join(base_path, 'gt0.png') ), dtype=float) / 255, 0, 1)
+gt = np.clip(np.asarray(Image.open( os.path.join(base_path, '320_depth0.png') ), dtype=float) / 255, 0, 1)
+print(gt.shape)
 # downsample
-gt = resize(gt, get_shape_depth(halved=True), preserve_range=True, mode='reflect', anti_aliasing=True )
+gt = resize(gt, get_shape_depth(halved=False), preserve_range=True, mode='reflect', anti_aliasing=True )
 # print_info(gt)
 
 # Compute results
-output = predict(model, inputs)
-# print_info(output)
-
-metrics = compute_errors(gt, np.reshape(output, get_shape_depth(halved=True)))
+disps, recons = predict(model, inputs)
+left_disp = disps[:,0]
+print_info(left_disp)
+print(left_disp.shape)
+pass
+metrics = compute_errors(np.reshape(gt, get_shape_depth(halved=False)), np.reshape(left_disp, get_shape_depth(halved=False)))
 print("{:>10}, {:>10}, {:>10}, {:>10}, {:>10}, {:>10}".format('a1', 'a2', 'a3', 'rel', 'rms', 'log_10'))
 print("{:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}".format(metrics[0],metrics[1],metrics[2],metrics[3],metrics[4],metrics[5]))
 
@@ -59,7 +68,7 @@ print("{:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}".format(metric
 #matplotlib.use('TkAgg')   
 
 # Display results
-viz = display_images(output.copy(), inputs.copy(), is_colormap=False)
+viz = display_images(left_disp.copy(), inputs.copy(), gt=[gt], is_colormap=False)
 split_path = os.path.normpath(args.input).split('/')
 if len(split_path) < 2: path = 'test.png'
 else: path = split_path[-2] + '_pred.png'
