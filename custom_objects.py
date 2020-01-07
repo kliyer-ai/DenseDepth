cdb61bfc363491b050507dfef17145681fbfe5b3
Nick Stracke <nick.stracke@web.de>
2020-01-07 09:28:07 +0100

    add warp function to custom objects

--- custom_objects.py
+++ custom_objects.py
@@ -1,8 +1,13 @@
 from loss import disparity_loss_function, reconstruction_loss_function
 from layers import BilinearUpSampling2D
+import tensorflow as tf
+from bilinear_sampler import generate_image_left, generate_image_right
 
 custom_objects = {
             'BilinearUpSampling2D': BilinearUpSampling2D, 
             'disparity_loss_function': disparity_loss_function,
-            'reconstruction_loss_function': reconstruction_loss_function
+            'reconstruction_loss_function': reconstruction_loss_function,
+            'tf': tf,
+            'generate_image_left': generate_image_left,
+            'generate_image_right': generate_image_right,
             }
