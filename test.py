361319c08385bc9584c39c065a80d46b055ce3e9
Nick Stracke <nick.stracke@web.de>
2020-01-11 15:15:13 +0100

    adjust test to new data format

--- test.py
+++ test.py
@@ -21,13 +21,12 @@ def print_info(img):
     print('max', img.max())
     print('mean', img.mean())
 
-# Argument Parser
 parser = argparse.ArgumentParser(description='High Quality Monocular Depth Estimation via Transfer Learning')
 parser.add_argument('--model', default='nyu.h5', type=str, help='Trained Keras model file.')
 parser.add_argument('--input', default='examples/*.png', type=str, help='Input filename or folder.')
 args = parser.parse_args()
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '2'
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 
 print('Loading model...')
 
@@ -44,14 +43,16 @@ simple_model = Model(input=input_left, output=disp_left)
 
 # Input images
 inputs = load_images( glob.glob(args.input) )
+inputs.append(np.array([1]))
 
 # GT
 base_path, _ = os.path.split(args.input)
-# gt = np.clip(np.asarray(Image.open( os.path.join(base_path, 'gt0.png') ), dtype=float) / 255, 0, 1)
-gt = np.clip(np.asarray(Image.open( os.path.join(base_path, '320_depth0.png') ), dtype=float) / 255, 0, 1)
+gt = np.clip(np.asarray(Image.open( os.path.join(base_path, 'gt0.png') ), dtype=float) / 255, 0, 1)
+# gt = np.clip(np.asarray(Image.open( os.path.join(base_path, '320_depth0.png') ), dtype=float) / 255, 0, 1)
 print(gt.shape)
 # downsample
-gt = resize(gt, get_shape_depth(halved=False), preserve_range=True, mode='reflect', anti_aliasing=True )
+is_halved=True
+gt = resize(gt, get_shape_depth(halved=is_halved), preserve_range=True, mode='reflect', anti_aliasing=True )
 # print_info(gt)
 
 # Compute results
@@ -59,16 +60,20 @@ disps, recons = predict(model, inputs)
 left_disp = disps[:,0]
 print_info(left_disp)
 print(left_disp.shape)
-pass
-metrics = compute_errors(np.reshape(gt, get_shape_depth(halved=False)), np.reshape(left_disp, get_shape_depth(halved=False)))
+
+metrics = compute_errors(np.reshape(gt, get_shape_depth(halved=is_halved)), np.reshape(left_disp, get_shape_depth(halved=is_halved)))
 print("{:>10}, {:>10}, {:>10}, {:>10}, {:>10}, {:>10}".format('a1', 'a2', 'a3', 'rel', 'rms', 'log_10'))
 print("{:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}".format(metrics[0],metrics[1],metrics[2],metrics[3],metrics[4],metrics[5]))
 
 #matplotlib problem on ubuntu terminal fix
 #matplotlib.use('TkAgg')   
 
+print(inputs[0].shape)
+print(inputs[1].shape)
+print(inputs[2].shape)
+
 # Display results
-viz = display_images(left_disp.copy(), inputs.copy(), gt=[gt], is_colormap=False)
+viz = display_images(left_disp.copy(), inputs[:2], gt=[gt], is_colormap=False)
 split_path = os.path.normpath(args.input).split('/')
 if len(split_path) < 2: path = 'test.png'
 else: path = split_path[-2] + '_pred.png'
