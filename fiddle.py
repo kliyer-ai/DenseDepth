1f8b35067a1f0e4e68c1d8a6f223dd9f3032b76d
Nick Stracke <nick.stracke@web.de>
2020-01-12 15:37:18 +0100

    remove unused files

--- fiddle.py
+++
@@ -1,6 +0,0 @@
-from skimage.transform import resize
-import numpy as np
-
-X = np.zeros((480,640,3))
-X = resize(X, (240, int(240*4/3)), preserve_range=True, mode='reflect', anti_aliasing=True )
-print(X.shape)
